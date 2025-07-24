81984fe52818a1b26ed3c8dfed1fa8ea9f24fcb5
Firoz aalam <36567191+rozkhan@users.noreply.github.com>
2025-07-24 14:54:47 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/controller/Admin.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Admin {
+	
+	
+	@GetMapping("/test")
+	public String test() {
+		
+		return "Testing...";
+	}
+
+}
